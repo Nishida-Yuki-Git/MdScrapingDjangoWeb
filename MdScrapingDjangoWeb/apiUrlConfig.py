b8d2c5e3cd80d1f62f666d2916a3a7d6c125cc63
Nishida-Yuki-Git <yellow.heart788@gmail.com>
2022-03-22 22:52:56 +0900

    1

--- MdScrapingDjangoWeb/apiUrlConfig.py
+++ MdScrapingDjangoWeb/apiUrlConfig.py
@@ -5,7 +5,7 @@ class ApiUrlConfig():
         ##ローカル開発環境
         #self.apiUrl = 'http://127.0.0.1:8000'
         ##LAN内サーバー検証(DHCPのため、適宜変更)
-        #self.apiUrl = 'http://192.168.2.102:80'
+        #self.apiUrl = 'http://192.168.2.101:80'
         ##本番環境用
         self.apiUrl = 'http://117.102.204.252:80'
 
